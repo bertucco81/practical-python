1c6c40e2b6f09d13ee28794e7fc9672e727e80de
bertucco81 <robert@rainland-cr.com>
2023-10-12 21:09:03 -0500

    update

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,15 @@
 # bounce.py
 #
 # Exercise 1.5
+
+height = 100
+bounce_back = height * 0.6
+bounce_count = 1
+
+while bounce_count < 11:
+    print(bounce_count, round(bounce_back, 4))
+    bounce_count = bounce_count + 1
+    bounce_back = bounce_back * 0.6
+
+
+
---
+++ Work/drafts.py
@@ -0,0 +1,8 @@
+a = 'Hello World'
+def myfunction():
+    if a.startswith('H'):
+        print('Correct')
+    else:
+        print('False')
+
+myfunction()
